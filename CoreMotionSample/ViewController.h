91ebf7773646237687e1eb3f7149ee3db6089035
Rex <rex.huang@ehsn.com.tw>
2017-09-24 12:46:28 +0800

    Initial Commit

---
+++ CoreMotionSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreMotionSample
+//
+//  Created by Rex on 2017/9/24.
+//  Copyright © 2017年 Rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreMotionSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreMotionSample
+//
+//  Created by Rex on 2017/9/24.
+//  Copyright © 2017年 Rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
